276d9ad376eb41d7be18ea3ae1d1140eaa683941
jackmcosta32 <joaomcosta32@gmail.com>
2025-02-06 21:43:45 -0300

    chore: adds path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
+import path from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
     port: 3000,
     open: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 });
