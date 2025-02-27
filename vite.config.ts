3a31995c7267478de9f894502ac0cd81c01b72fc
jackmcosta32 <joaomcosta32@gmail.com>
2025-02-27 20:56:27 -0300

    refactor: moves test suites outside of the package scope

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
+      '@tests': path.resolve(__dirname, './src/tests'),
       '@pacman': path.resolve(__dirname, './src/app/pacman'),
-      '@tests': path.resolve(__dirname, './src/packages/tests'),
       '@shared': path.resolve(__dirname, './src/packages/shared'),
       '@game-client': path.resolve(__dirname, './src/packages/game-client'),
       '@game-engine': path.resolve(__dirname, './src/packages/game-engine'),
