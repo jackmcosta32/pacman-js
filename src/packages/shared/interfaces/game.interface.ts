d6061f37fd85b17f9f4f085164ae6bcf81431762
jackmcosta32 <joaomcosta32@gmail.com>
2025-03-11 23:07:07 -0300

    feat: updates game interfaces

--- src/packages/shared/interfaces/game.interface.ts
+++ src/packages/shared/interfaces/game.interface.ts
@@ -1,12 +1,16 @@
+import type { IEvent } from './event.interface';
 import type { Callback } from '@shared/types/util.type';
-import type { IInputEvent } from '@shared/interfaces/event.interface';
 import type { IObserver } from '@shared/interfaces/observer.interface';
 import type { ISerializedScene } from '@game-engine/interfaces/scene.interface';
+
+export interface IGameState {
+  events: IEvent[];
+}
 
 export interface IGame extends IObserver<Callback<ISerializedScene>> {
   init(): void;
   start(): void;
   update(): void;
   destroy(): void;
-  readInputs(inputs: IInputEvent[]): void;
+  readClientEvent(event?: IEvent): void;
 }
