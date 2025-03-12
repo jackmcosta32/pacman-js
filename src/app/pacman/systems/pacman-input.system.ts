fd831102985b45c14c8a70a7dfadf52e05f39208
jackmcosta32 <joaomcosta32@gmail.com>
2025-03-11 23:05:43 -0300

    feat: rewords the pacman input system [wip]

--- src/app/pacman/systems/pacman-input.system.ts
+++ src/app/pacman/systems/pacman-input.system.ts
@@ -1,34 +1,71 @@
+import { System } from '@game-engine/core/system';
 import type { IEntity } from '@game-engine/interfaces/entity.interface';
+import { PACMAN_SYSTEM } from '@pacman/constants/pacman-system.constant';
+import type { ICoordinate } from '@shared/interfaces/coordinate.interface';
+import type { ISceneState } from '@game-engine/interfaces/scene.interface';
+import { PACMAN_EVENT_TYPE } from '@pacman/constants/pacman-event.constant';
+import { ControlComponent } from '@game-engine/components/control.component';
+import { PositionComponent } from '@game-engine/components/position.component';
+import { PacmanActorComponent } from '@pacman/components/pacman-actor.component';
+import type { IPacmanMoveEvent } from '@pacman/interfaces/pacman-event.interface';
+import { PACMAN_ACTOR_DIRECTION, PACMAN_ACTOR_MOVEMENT_STATE } from '@pacman/constants/pacman-actor.constant';
+import type { IPacmanActorDirection, IPacmanActorMovementState } from '@pacman/interfaces/pacman-actor.interface';
 
-export interface IInputSystemConstructor {
-  movementSystem: 
-}
+export class PacmanInputSystem extends System {
+  public static readonly id = PACMAN_SYSTEM.INPUT;
 
-export class InputSystem {
+  public updateEntityMovementState(
+    entity: IEntity,
+    movementState: IPacmanActorMovementState,
+    direction: IPacmanActorDirection,
+    elapsed: number,
+  ) {
+    const controlComponent = entity.getComponent(ControlComponent);
 
-  private entitiesManager: unknown;
-  private movementSystem: unknown;
+    if (!controlComponent) return;
 
-  private handleGameEvent(event: IPacmanGameEvent, target: IEntity) {
-    switch (event.type) {
-      case PACMAN_EVENT.MOVE_UP:
-        this.movementSystem.move(target, PACMAN_MOVEMENT.UP);
+    const positionComponent = entity.getComponent(PositionComponent);
+    const actorComponent = entity.getComponent(PacmanActorComponent);
+
+    if (!positionComponent || !actorComponent) return;
+
+    actorComponent.updateDirection(direction);
+    actorComponent.updateMovementState(movementState);
+
+    if (movementState === PACMAN_ACTOR_MOVEMENT_STATE.IDLE) return;
+
+    const currentPosition = positionComponent.position;
+
+    let nextPosition: ICoordinate;
+    const distance = actorComponent.speed * elapsed;
+
+    switch (actorComponent.direction) {
+      case PACMAN_ACTOR_DIRECTION.UP:
+        nextPosition = { x: currentPosition.x, y: currentPosition.y - distance };
         break;
-      case PACMAN_EVENT.MOVE_DOWN:
-        this.movementSystem.move(target, PACMAN_MOVEMENT.DOWN);
+      case PACMAN_ACTOR_DIRECTION.DOWN:
+        nextPosition = { x: currentPosition.x, y: currentPosition.y + distance };
         break;
-      case PACMAN_EVENT.MOVE_LEFT:
-        this.movementSystem.move(target, PACMAN_MOVEMENT.LEFT);
+      case PACMAN_ACTOR_DIRECTION.LEFT:
+        nextPosition = { x: currentPosition.x - distance, y: currentPosition.y };
         break;
-      case PACMAN_EVENT.MOVE_RIGHT:
-        this.movementSystem.move(target, PACMAN_MOVEMENT.RIGHT);
+      default:
+        nextPosition = { x: currentPosition.x + distance, y: currentPosition.y };
         break;
     }
+
+    positionComponent.updatePosition(nextPosition);
   }
 
-  public update(input: IPacmanGameEvent[]) {
-    if (!eventStream.length) return;
+  public update(sceneState: ISceneState) {
+    sceneState.events.forEach((event) => {
+      if (event.type !== PACMAN_EVENT_TYPE.MOVE) return;
 
-    eventStream.forEach((event) => {});
+      const { direction, movementState } = event as IPacmanMoveEvent;
+
+      sceneState.entityManager.getEntities().forEach((entity) => {
+        this.updateEntityMovementState(entity, movementState, direction, sceneState.elapsed);
+      });
+    });
   }
 }
