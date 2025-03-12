552be49e65525e10c307cd3a26888ad5ac931334
jackmcosta32 <joaomcosta32@gmail.com>
2025-03-11 23:05:05 -0300

    feat: updates the pacman animation system to handle the time controlled animations

--- src/app/pacman/systems/pacman-animation.system.ts
+++ src/app/pacman/systems/pacman-animation.system.ts
@@ -1,9 +1,14 @@
+import { System } from '@game-engine/core/system';
 import type { IEntity } from '@game-engine/interfaces/entity.interface';
+import { PACMAN_SYSTEM } from '@pacman/constants/pacman-system.constant';
+import type { ISceneState } from '@game-engine/interfaces/scene.interface';
 import { SpriteComponent } from '@game-engine/components/sprite.component';
 import { PacmanActorComponent } from '@pacman/components/pacman-actor.component';
 
-export class PacmanAnimationSystem {
-  public update(entity: IEntity) {
+export class PacmanAnimationSystem extends System {
+  public static readonly id = PACMAN_SYSTEM.ANIMATION;
+
+  public updateEntityAnimationFrame(entity: IEntity, elapsed: number) {
     const spriteComponent = entity.getComponent(SpriteComponent);
 
     if (!spriteComponent) return;
@@ -14,6 +19,10 @@ export class PacmanAnimationSystem {
       spriteComponent.updateSpriteFrames(actorComponent.spriteFrames);
     }
 
-    spriteComponent.updateAnimationFrame();
+    spriteComponent.updateAnimationFrame(elapsed);
+  }
+
+  public update(sceneState: ISceneState) {
+    sceneState.entityManager.forEachEntity((entity) => this.updateEntityAnimationFrame(entity, sceneState.elapsed));
   }
 }
